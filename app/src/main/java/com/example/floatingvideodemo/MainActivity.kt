65b6927b25f0ad08ad634b620584e2ec8e3394ad
Pritesh Nikam <pritesh.nikam@coditas.com>
2023-05-09 17:25:25 +0530

    handle plane discover instructions and play video vertically

--- app/src/main/java/com/example/floatingvideodemo/MainActivity.kt
+++ app/src/main/java/com/example/floatingvideodemo/MainActivity.kt
@@ -4,9 +4,9 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.HitResult
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
-import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.Color
 import com.google.ar.sceneform.rendering.ExternalTexture
@@ -14,70 +14,78 @@ import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
-    var videoRenderable: ModelRenderable? = null
-    private val HEIGHT = 0.25f
-    private val VIDEO_HEIGHT_METERS = 0.45f
+    companion object {
+        const val HEIGHT = 0.25f
+    }
 
+    private var videoRenderable: ModelRenderable? = null
     private lateinit var arFragment: ArFragment
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var anchorNode: AnchorNode
     private lateinit var videoTexture: ExternalTexture
-    private var taped: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.video)
+        setContentView(R.layout.activity_main)
+        arFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment
+        arFragment.planeDiscoveryController.hide()
+        arFragment.planeDiscoveryController.setInstructionView(null)
+      //  arFragment.arSceneView.planeRenderer.isEnabled = false
+
         videoTexture = ExternalTexture()
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.ted)
         mediaPlayer.setSurface(videoTexture.surface)
         mediaPlayer.isLooping = true
 
         ModelRenderable.builder()
-            .setSource(this, R.raw.video_screen)
+            .setSource(this, R.raw.chroma_key_video)
             .build()
-            .thenAccept { modelRenderable ->
-                videoRenderable = modelRenderable
-                videoRenderable?.material?.setExternalTexture("videoTexture", videoTexture)
-                videoRenderable?.material
-                    ?.setFloat4("keyColor", Color(0.01843f, 1.0f, 0.098f))
+            .thenAccept { renderable: ModelRenderable ->
+                videoRenderable = renderable
+                renderable.material.setExternalTexture("videoTexture", videoTexture)
+                renderable.material.setFloat4(
+                    "keyColor",
+                    Color(0.1843f, 1.0f, 0.098f)
+                )
+            }
+            .exceptionally {
+                Toast.makeText(this, "Unable to load video renderable", Toast.LENGTH_LONG).show()
+                null
+            }
+        arFragment.setOnTapArPlaneListener { hitResult: HitResult, _, _ ->
+            if (videoRenderable == null) {
+                return@setOnTapArPlaneListener
             }
 
-        arFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment
+            val anchor = hitResult.createAnchor()
+            anchorNode =
+                AnchorNode(anchor)
+            anchorNode.setParent(arFragment.arSceneView.scene)
 
-        if (taped) {
-            mediaPlayer.pause()
-        } else {
-            arFragment.setOnTapArPlaneListener { hitResult, plane, _ ->
-                taped = true
-                anchorNode = AnchorNode(hitResult.createAnchor())
-                anchorNode.setParent(arFragment.arSceneView.scene)
-                val videoNode = Node()
+            val videoNode = Node()
+            videoNode.setParent(anchorNode)
 
-                videoNode.setParent(anchorNode)
-                videoNode.localRotation = Quaternion.axisAngle(Vector3(0.0f, 1.0f, 0.0f), -270.0f)
+            val videoWidth = mediaPlayer.videoWidth.toFloat()
+            val videoHeight = mediaPlayer.videoHeight.toFloat()
+            videoNode.localScale = Vector3(
+                HEIGHT * (videoWidth / videoHeight),
+                HEIGHT,
+                1.0f
+            )
 
-                val videoWidth = mediaPlayer.videoWidth.toFloat()
-                val videoHeight = mediaPlayer.videoHeight.toFloat()
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
 
-                videoNode.localScale = Vector3(
-                    VIDEO_HEIGHT_METERS * (videoWidth / videoHeight), VIDEO_HEIGHT_METERS, 1.0f
-                )
-
-                if (!mediaPlayer.isPlaying ) { // && plane.type == com.google.ar.core.Plane.Type.VERTICAL
-                    mediaPlayer.start()
-                    videoTexture.surfaceTexture
-                        .setOnFrameAvailableListener { surfaceTexture ->
-                            videoNode.renderable = videoRenderable
-                            surfaceTexture.setOnFrameAvailableListener(null)
-                        }
-                } else {
-                    Toast.makeText(this,"not a vertical surface",Toast.LENGTH_SHORT).show()
-                    mediaPlayer.pause()
-                }
-
-                anchorNode.localScale = Vector3(HEIGHT * (videoWidth / videoHeight), HEIGHT, 0.25f)
-                arFragment.arSceneView.scene.addChild(anchorNode)
+                videoTexture
+                    .surfaceTexture
+                    .setOnFrameAvailableListener {
+                        videoNode.renderable = videoRenderable
+                        videoTexture.surfaceTexture.setOnFrameAvailableListener(null)
+                    }
+            } else {
+                videoNode.renderable = videoRenderable
             }
         }
     }
