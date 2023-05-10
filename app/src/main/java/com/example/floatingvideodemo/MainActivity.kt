b15cf9979725d955a9b84002db5845bb6060f4f3
Pritesh Nikam <pritesh.nikam@coditas.com>
2023-05-10 14:21:21 +0530

    handle pinch zoom of video

--- app/src/main/java/com/example/floatingvideodemo/MainActivity.kt
+++ app/src/main/java/com/example/floatingvideodemo/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.floatingvideodemo
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.ar.core.HitResult
+import com.google.ar.core.Config
 import com.google.ar.sceneform.AnchorNode
-import com.google.ar.sceneform.Node
-import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.Color
 import com.google.ar.sceneform.rendering.ExternalTexture
 import com.google.ar.sceneform.rendering.ModelRenderable
@@ -23,15 +24,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var anchorNode: AnchorNode
     private lateinit var videoTexture: ExternalTexture
+    private lateinit var gestureDetector: GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
         arFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment
         arFragment.planeDiscoveryController.hide()
         arFragment.planeDiscoveryController.setInstructionView(null)
-      //  arFragment.arSceneView.planeRenderer.isEnabled = false
+        arFragment.arSceneView.planeRenderer.isEnabled = false
+
+        arFragment.arSceneView.session?.config?.let { config ->
+            config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+            config.focusMode = Config.FocusMode.AUTO
+        }
 
         videoTexture = ExternalTexture()
 
@@ -54,7 +60,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Unable to load video renderable", Toast.LENGTH_LONG).show()
                 null
             }
-        arFragment.setOnTapArPlaneListener { hitResult: HitResult, _, _ ->
+
+/*        arFragment.setOnTapArPlaneListener { hitResult: HitResult, _, _ ->
             if (videoRenderable == null) {
                 return@setOnTapArPlaneListener
             }
@@ -64,7 +71,7 @@ class MainActivity : AppCompatActivity() {
                 AnchorNode(anchor)
             anchorNode.setParent(arFragment.arSceneView.scene)
 
-            val videoNode = Node()
+            val videoNode = TransformableNode(arFragment.transformationSystem)
             videoNode.setParent(anchorNode)
 
             val videoWidth = mediaPlayer.videoWidth.toFloat()
@@ -87,15 +94,66 @@ class MainActivity : AppCompatActivity() {
             } else {
                 videoNode.renderable = videoRenderable
             }
-        }
+            arFragment.arSceneView.scene.addChild(anchorNode)
+            videoNode.select()
+        }*/
+
+        gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                val x = e.x
+                val y = e.y
+
+                Log.d("gestureDetector", "user taped")
+                Toast.makeText(this@MainActivity, "tap", Toast.LENGTH_SHORT).show()
+
+                val hitResult = arFragment.arSceneView.arFrame?.hitTest(x, y)?.firstOrNull()
+
+      /*          if (hitResult != null) {
+                    Toast.makeText(this@MainActivity, "tap", Toast.LENGTH_SHORT).show()
+
+                    val anchor = hitResult.createAnchor()
+                    anchorNode =
+                        AnchorNode(anchor)
+                    anchorNode.setParent(arFragment.arSceneView.scene)
+
+                    val videoNode = TransformableNode(arFragment.transformationSystem)
+                    videoNode.setParent(anchorNode)
+
+                    val videoWidth = mediaPlayer.videoWidth.toFloat()
+                    val videoHeight = mediaPlayer.videoHeight.toFloat()
+                    videoNode.localScale = Vector3(
+                        HEIGHT * (videoWidth / videoHeight),
+                        HEIGHT,
+                        1.0f
+                    )
+
+                    if (!mediaPlayer.isPlaying) {
+                        mediaPlayer.start()
+
+                        videoTexture
+                            .surfaceTexture
+                            .setOnFrameAvailableListener {
+                                videoNode.renderable = videoRenderable
+                                videoTexture.surfaceTexture.setOnFrameAvailableListener(null)
+                            }
+                    } else {
+                        videoNode.renderable = videoRenderable
+                    }
+                    arFragment.arSceneView.scene.addChild(anchorNode)
+                    videoNode.select()
+                }*/
+                return super.onSingleTapUp(e)
+            }
+        })
     }
 
     override fun onPause() {
         super.onPause()
         mediaPlayer.pause()
     }
-
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        Log.d("touchEvent","user taped")
+        gestureDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
 }
-
-
-
